00eff545d7a937fbe7bdced512de8e284f9b2b1a
Roberto <barbosaddt337@hotmail.com>
2025-07-23 12:40:14 -0300

    Inicializador de salas vigilias

--- demo/src/main/java/gerenciador/service/VigiliaService.java
+++ demo/src/main/java/gerenciador/service/VigiliaService.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import jakarta.annotation.PostConstruct;
 import java.util.List;
 import java.util.Optional;
 
@@ -80,5 +81,10 @@ public class VigiliaService {
 
     public void inicializarSalasVigilia() {
         partidaService.inicializarPartidasVigilia();
+    }
+
+    @PostConstruct
+    public void initVigiliaSalas() {
+        inicializarSalasVigilia();
     }
 } 
